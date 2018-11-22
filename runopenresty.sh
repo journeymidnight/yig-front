8763fcd30ce55b4af2ee25fa9c0b5fd0726586bb
Shaoxiong Li <dahefanteng@gmail.com>
2018-11-22 17:28:26 +0800

    Fix wrong image bug

--- runopenresty.sh
+++ runopenresty.sh
@@ -4,4 +4,4 @@ sudo docker run --rm -d --name yig-front \
                  -v ${BASEDIR}:/var/log/nginx/ \
                  --net=integrate_vpcbr \
                  --ip 10.5.0.100 \
-                 journeymidnight/openresty
+                 journeymidnight/yig-front
--- runprometheus.sh
+++ runprometheus.sh
@@ -1,5 +1,5 @@
 BASEDIR=$(pwd)
-sudo docker run  --name prometheus \
+sudo docker run --rm -d --name prometheus \
 		 -p 9090:9090 \
                  -v ${BASEDIR}/prometheus.yml:/etc/prometheus/prometheus.yml \
                  --net=integrate_vpcbr \
