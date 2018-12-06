5e60dc0bd57231318502ada5d3420831292165d4
dongmao zhang <deanraccoon@gmail.com>
2018-12-06 18:01:08 +0800

    Update bucket.lua

--- bucket.lua
+++ bucket.lua
@@ -3,7 +3,7 @@ ngx.log(ngx.NOTICE, "HOST=", host)
 local subdomain = string.match(host, "(.+).s3%.test%.com")--部署到不同的集群，需要修改对应的域名匹配字段
 ngx.log(ngx.NOTICE, "subdomain1=", subdomain)
 if subdomain == nil then
-        subdomain = string.match(ngx.var.uri, "/(.+)/.+")
+        subdomain = string.match(ngx.var.uri, "/([^/]+)/*")
         ngx.log(ngx.NOTICE, "subdomain2=", subdomain)
 end
 if subdomain == "purge" then
